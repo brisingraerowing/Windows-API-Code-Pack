1bb8d3f9608c6a934c7c300f5a329caaa8756c89
pierresprim <wincopies@wincopies.com>
2020-02-01 13:46:20 +0100

    Update PortableDevices namespaces

--- source/WindowsAPICodePack/Win32Native/WindowsPortableDevices/ClientInterfaces/IPortableDeviceContent.cs
+++ source/WindowsAPICodePack/Win32Native/WindowsPortableDevices/ClientInterfaces/IPortableDeviceContent.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAPICodePack.Win32Native;
 using Microsoft.WindowsAPICodePack.Win32Native.PortableDevices;
 using Microsoft.WindowsAPICodePack.Win32Native.PortableDevices.PropertySystem;
-using Microsoft.WindowsAPICodePack.Win32Native.PortableDevices.PropertySystem.Resources;
+using Microsoft.WindowsAPICodePack.Win32Native.PortableDevices.ResourceSystem;
 
 namespace Microsoft.WindowsAPICodePack.Win32Native.PortableDevices
 {
